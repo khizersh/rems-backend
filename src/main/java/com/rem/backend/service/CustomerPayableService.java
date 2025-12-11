b20692af9c7b33267ec3d574e9912515e9be3076
mawizakrama <mawizakrama@gmail.com>
2025-12-12 03:00:12 +0500

    all central flow completed for cancel booking along payable details get endpoints created
    nothing has been tested by me

--- src/main/java/com/rem/backend/service/CustomerPayableService.java
+++ src/main/java/com/rem/backend/service/CustomerPayableService.java
@@ -17,8 +17,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
