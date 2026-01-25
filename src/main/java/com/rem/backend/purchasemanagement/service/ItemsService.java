d312e28d01db34e36da4896de678cbbd913b09b4
KhizerYounus <khizer.younus@simpaisa.com>
2026-01-25 13:34:53 +0500

    init

---
+++ src/main/java/com/rem/backend/purchasemanagement/service/ItemsService.java
@@ -0,0 +1,9 @@
+package com.rem.backend.purchasemanagement.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ItemsService {
+}
